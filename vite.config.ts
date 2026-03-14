250d894293f38e791d833afb712cea643a2279ca
Lucas Brandi <lgbrandidantas17@gmail.com>
2026-03-14 19:43:23 -0300

    chore: configuracao final de lint e husky

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true, // Permite usar 'describe', 'it', 'expect' sem importar
+    environment: 'jsdom', // Simula o navegador para testar componentes React
+    setupFiles: './src/setupTests.ts', // Arquivo para configurar o jest-dom
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
