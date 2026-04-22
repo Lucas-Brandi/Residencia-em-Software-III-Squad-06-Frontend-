48c9dc8e2c4ec8fafc75d9eb1578492039395efa
Karolyne Cunha - GitHub <karolswift59@gmail.com>
2026-04-22 13:25:53 -0300

    Telas Login, Recuperar Senha e Cadastro + Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,16 @@ import PullRequests from '@/pages/pull-requests';
 import { Regras } from '@/pages/regras';
 import { Admin } from '@/pages/admin';
 import { Perfil } from '@/pages/perfil';
+import { Login } from '@/pages/login';
+import { Register } from '@/pages/cadastro'; 
+import { ForgotPassword } from '@/pages/recuperarSenha'; 
 
 function App() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Register />} />
+      <Route path="/recuperarSenha" element={<ForgotPassword />} />
       <Route path="/" element={<Navigate to="/dashboard" />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/prs" element={<PullRequests />} />
